4e40b8e7926d36426d5abe4cb81988ee0f05610f
Lucas Paganini de Moura <lucaspaganinidm@gmail.com>
2018-10-19 14:20:49 -0300

    minimal reproduction of the issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,21 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  template: `<h3>{{ message }}</h3>`
+  selector: "app-home",
+  template: `
+    <h3>Problem server side rendering angular imask</h3>
+    <input type="tel" [imask]="phoneMask">
+  `
 })
-export class HomeComponent implements OnInit {
-  public message: string;
-
-  constructor() {}
-
-  ngOnInit() {
-    this.message = 'Hello';
-  }
+export class HomeComponent {
+  phoneMask = {
+    mask: [
+      {
+        mask: "(00) 0000-0000"
+      },
+      {
+        mask: "(00) 00000-0000"
+      }
+    ]
+  };
 }
